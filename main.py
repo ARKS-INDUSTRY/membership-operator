ca7f356720373b99bdbee11797dc3318d774a678
ARKS-INDUSTRY <59530718+ARKS-INDUSTRY@users.noreply.github.com>
2021-09-04 18:18:51 +0530

    membership.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+''' writen by rakesn '''
+
 list_of_lang = ['python', 'java', 'javascript', 'html', 'css', 'sass', 'swift', 'kotlin', 'r', 'c', 'c++']
 
 add = str(input("enter the programimg lang: ")).lower()
@@ -6,3 +8,7 @@ if add in list_of_lang:
   print('Best lang for programming is '+ add )
 else:
   print("opps! something went wrong, check your spelling")
+  
+  
+  
+  
